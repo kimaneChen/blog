a082308777a0d68cec14470761a487b050e8388f
Yifan Zhang <41891412+simplypa@users.noreply.github.com>
2023-02-06 13:59:07 +1100

    update logo (#46)

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,8 +4,7 @@ import logo from './assets/logo-chuck.svg'
 
 const Logo: FC = () => (
   <section className="flex gap-3">
-    <Image src={logo} alt="ChuckRoo" width={88} height={20} />
-    Let&apos;s Comment
+    <Image src={logo} alt="ChuckRoo" width={136} height={52} />
   </section>
 )
 
