28623452d3f2bf03c87b334eb2a8a0835b504e35
Yifan Zhang (Mitchell) <41891412+simplypa@users.noreply.github.com>
2023-02-24 21:08:36 +1100

    bug/sc-957/bug-all-blogs-page (#106)
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- src/application/BlogsPage/BlogsPage.tsx
+++ src/application/BlogsPage/BlogsPage.tsx
@@ -7,7 +7,6 @@ import { NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
 import useSWR from 'swr'
-import Container from '@/components/Container'
 import useBlogs from './hooks/useBlogs'
 
 const BlogsPage: NextPage = () => {
@@ -26,13 +25,10 @@ const BlogsPage: NextPage = () => {
       </Head>
 
       <Layout>
-        <Container>
-          <section className="flex">
-            <div className="w-[300px] ml-6 border-r">
-              <div className="pr-5 my-9">
-                <div className="font-medium">Filters</div>
-                <div className="mt-3" />
-              </div>
+        <section className="flex">
+          <div className="w-[300px] border-r">
+            <div className="pr-5 mt-9">
+              <div>Filters</div>
               <div>
                 {tags ? (
                   <TagsFilter
@@ -53,38 +49,38 @@ const BlogsPage: NextPage = () => {
                 )}
               </div>
             </div>
+          </div>
 
-            <div className="grow py-9 px-5 mx-3">
-              <div className="mb-4">
-                <h1 className="text-3xl font-bold">All Blogs</h1>
-              </div>
-              <div className="text-on-background">See what&apos;s new on the blog</div>
-              <div className="mt-6">
-                {blogs.map((blog) => (
-                  <div key={blog.id} className="mb-6">
-                    <BlogOverview
-                      id={blog.id}
-                      title={blog.title}
-                      date={blog.createdAt}
-                      tags={blog.tags}
-                      avatar={{
-                        src: blog.user?.image,
-                        alt: blog.user?.name || 'Unknown user',
-                      }}
-                    >
-                      {blog.description}
-                    </BlogOverview>
-                  </div>
-                ))}
-              </div>
-              <div>
-                <LoadMoreButton hasMore={!isLoadMoreDisabled} onLoadMore={() => setSize(size + 1)}>
-                  More Blogs
-                </LoadMoreButton>
-              </div>
+          <div className="grow py-9 px-5 mx-3">
+            <div className="mb-4">
+              <h1 className="text-3xl font-bold">All Blogs</h1>
             </div>
-          </section>
-        </Container>
+            <div className="text-on-background">See what&apos;s new on the blog</div>
+            <div className="mt-6">
+              {blogs.map((blog) => (
+                <div key={blog.id} className="mb-6">
+                  <BlogOverview
+                    id={blog.id}
+                    title={blog.title}
+                    date={blog.createdAt}
+                    tags={blog.tags}
+                    avatar={{
+                      src: blog.user?.image,
+                      alt: blog.user?.name || 'Unknown user',
+                    }}
+                  >
+                    {blog.description}
+                  </BlogOverview>
+                </div>
+              ))}
+            </div>
+            <div>
+              <LoadMoreButton hasMore={!isLoadMoreDisabled} onLoadMore={() => setSize(size + 1)}>
+                More Blogs
+              </LoadMoreButton>
+            </div>
+          </div>
+        </section>
       </Layout>
     </>
   )
