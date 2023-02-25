f0874554da09ce0c468ba7115e012000dc3e5325
Kimane <chenjx1024@outlook.com>
2023-02-25 21:30:50 +1000

    fix/solve the width

--- src/application/BlogViewPage/BlogViewPage.tsx
+++ src/application/BlogViewPage/BlogViewPage.tsx
@@ -7,6 +7,7 @@ import BackLink from './components/BackLink'
 import BlogTitle from './components/BlogTitle'
 import Content from './components/Content'
 import AddComments from './components/AddComments'
+import Comments from './components/Comments'
 
 const BlogViewPage: NextPage = () => {
   const router = useRouter()
@@ -21,9 +22,12 @@ const BlogViewPage: NextPage = () => {
         <BackLink />
         <BlogTitle />
         <Content />
-        <hr className="w-screen"/>
-        <AddComments />
       </Layout>
+
+      <hr />
+      <Container>
+        <AddComments />
+      </Container>
     </>
   )
 }
