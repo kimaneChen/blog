73a5b8a206764b7e08d1369f16d0827832f3151e
QueenVivi <111481335+QueenVivi@users.noreply.github.com>
2023-03-03 16:12:55 +1100

    feat:redirect link to blogs page (#137)

--- src/application/BlogViewPage/components/BackLink/BackLink.tsx
+++ src/application/BlogViewPage/components/BackLink/BackLink.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { FiArrowLeft } from 'react-icons/fi'
 
 const BackLink: FC = () => (
-  <Link href="/" className="mt-1 py-3 flex items-center gap-1 text-sm text-on-background">
+  <Link href="/blogs" className="mt-1 py-3 flex items-center gap-1 text-sm text-on-background">
     <FiArrowLeft className="inline w-4 h-4" />
     Back to All Blogs
   </Link>
