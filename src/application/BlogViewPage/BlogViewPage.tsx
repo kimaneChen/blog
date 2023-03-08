7c7cfd01f8baa893cee34310e740811a10c53b12
kimaneChen <71416269+kimaneChen@users.noreply.github.com>
2023-03-08 18:01:43 +1000

    feature/sc-1189/homepage-blogs-loading (#147)
    * fix/solve the issue
    
    * fix/add another logic
    
    * fix/revert back
    
    * fix/solve the loading issue
    
    * fix/remove the user loading info
    
    * fix/change according to review comments
    
    * fix/change according to review comments
    
    * fix/merge the conflict
    
    * fix/change according review comments
    
    * fix/change according to review comments

--- src/application/BlogViewPage/BlogViewPage.tsx
+++ src/application/BlogViewPage/BlogViewPage.tsx
@@ -2,33 +2,41 @@ import { NextPage } from 'next'
 import useBlog from '@/hooks/useBlog'
 import Layout from '@/application/Layout'
 import Container, { Size } from '@/components/Container'
+import Loading from '@/components/Loading'
+import BackLink from './components/BackLink'
+import BlogTitle from './components/BlogTitle'
 import Content from './components/Content'
 import AddComments from './components/AddComments'
-import BlogTitle from './components/BlogTitle'
-import BackLink from './components/BackLink'
 import Comments from './components/Comments'
 import CommentsUserList from './components/CommentsUserList'
 
 const BlogViewPage: NextPage = () => {
   const { blog, isLoading } = useBlog()
-  if (isLoading) return <>Loading...</>
   if (!blog) return null
 
   return (
     <Layout>
-      <Container>
-        <BackLink />
-      </Container>
-      <Container size={Size.Narrow}>
-        <BlogTitle />
-        <Content />
-      </Container>
-      <hr />
-      <Container size={Size.Narrow}>
-        <AddComments />
-        <CommentsUserList />
-        <Comments />
-      </Container>
+      {isLoading ? (
+        <div className="h-[700px] flex items-center justify-center">
+          <Loading />
+        </div>
+      ) : (
+        <>
+          <Container>
+            <BackLink />
+          </Container>
+          <Container size={Size.Narrow}>
+            <BlogTitle />
+            <Content />
+          </Container>
+          <hr />
+          <Container size={Size.Narrow}>
+            <AddComments />
+            <CommentsUserList />
+            <Comments />
+          </Container>
+        </>
+      )}
     </Layout>
   )
 }
