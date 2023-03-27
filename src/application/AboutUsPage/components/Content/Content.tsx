c3b390eeaf334d0af446fd311b23fbc6df0e5016
Kimane <chenjx1024@outlook.com>
2023-03-27 12:07:37 +1000

    fix/change according to review comments

--- src/application/AboutUsPage/components/Content/Content.tsx
+++ src/application/AboutUsPage/components/Content/Content.tsx
@@ -5,7 +5,7 @@ import about from './assets/about.md'
 const Content: FC = () => (
   <article className="mb-[140px]">
     <h1 className="text-7xl font-extrabold text-center my-[180px]">About us</h1>
-    <div className="prose pt-24 max-w-[880px] text-lg">
+    <div className="prose pt-24 max-w-none text-lg">
       <LegalMarkdown>{about}</LegalMarkdown>
     </div>
   </article>
