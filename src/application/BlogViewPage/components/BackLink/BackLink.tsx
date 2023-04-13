e754a2c10f9c747370f653f9f78f9743a23c046f
Jerry Gao <gar@yeah.net>
2023-04-13 19:17:26 +1000

    Fix: Navigation back in Blogview (#211)

--- src/application/BlogViewPage/components/BackLink/BackLink.tsx
+++ src/application/BlogViewPage/components/BackLink/BackLink.tsx
@@ -1,12 +1,20 @@
 import { FC } from 'react'
-import Link from 'next/link'
 import { FiArrowLeft } from 'react-icons/fi'
+import { useRouter } from 'next/router'
 
-const BackLink: FC = () => (
-  <Link href="/blogs" className="mt-1 py-3 flex items-center gap-1 text-xs text-on-background">
-    <FiArrowLeft className="inline w-4 h-4" />
-    Back to All Blogs
-  </Link>
-)
+const BackLink: FC = () => {
+  const router = useRouter()
+
+  return (
+    <button
+      type="button"
+      onClick={() => router.back()}
+      className="mt-1 py-3 flex items-center gap-1 text-xs text-on-background cursor-pointer"
+    >
+      <FiArrowLeft className="inline w-4 h-4" />
+      Back to Blogs
+    </button>
+  )
+}
 
 export default BackLink
