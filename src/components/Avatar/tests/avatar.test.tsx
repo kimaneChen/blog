400940baebbae29e05779707a5a9c6bed33632fc
Kimane <chenjx1024@outlook.com>
2023-04-21 14:32:10 +1000

    fix/change the eslintrc to to avoid alarm caused by jest

---
+++ src/components/Avatar/tests/avatar.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import Avatar from '../Avatar'
+
+describe('Avatar test', () => {
+  it('renders a avatar with default setting', () => {
+    const avatar = render(<Avatar width={20} height={20} />)
+
+    const image = screen.getByAltText('User Avatar') as HTMLImageElement
+    expect(image.src).toContain('http://localhost/_next/image?url=%2Fimg.jpg&w=48&q=75')
+  })
+
+  it('renders a avatar with source link', () => {
+    const avatar = render(
+      <Avatar
+        width={20}
+        height={20}
+        src="https://www.shutterstock.com/image-vector/certificate-handout-creative-painting-line-icons-1979886350"
+        alt="out-source"
+      />
+    )
+
+    const image = screen.getByAltText('out-source') as HTMLImageElement
+    expect(image.src).toContain(
+      'http://localhost/_next/image?url=https%3A%2F%2Fwww.shutterstock.com%2Fimage-vector%2Fcertificate-handout-creative-painting-line-icons-1979886350&w=48&q=75'
+    )
+  })
+})
