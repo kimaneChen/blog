6747e7dd41a6d9b495d80965bb4ff773cf88a35b
Kimane <chenjx1024@outlook.com>
2023-04-27 14:58:24 +1000

    feat/add the localization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en-AU', 'es-US', 'zh-CN'],
+    defaultLocale: 'en-US',
+  },
   images: {
     remotePatterns: [
       {
