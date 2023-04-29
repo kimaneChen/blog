d8eaf3f37b98316601fd261475a086e9cb4d76ab
Kimane <chenjx1024@outlook.com>
2023-04-29 16:01:31 +1000

    fix/change according to review comments

--- src/components/Button/tests/button.test.tsx
+++ src/components/Button/tests/button.test.tsx
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react'
+/* eslint-disable testing-library/await-async-query */
+import { render, screen } from '@testing-library/react'
 import Button from '../Button'
 
 describe('Button test', () => {
   it('renders test with default setting', () => {
     const view = render(<Button>Test</Button>)
-    expect(view.findByRole('button', { name: 'Test' })).toBeInTheDocument
-    expect(button).toMatchSnapshot()
+    expect(screen.findByRole('button', { name: 'Test' })).toBeTruthy()
+    expect(view).toMatchSnapshot()
   })
 })
