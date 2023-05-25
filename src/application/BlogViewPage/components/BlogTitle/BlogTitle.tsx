0d9d6c52735f94d6ef7038b5b6fab0289f2a0b33
Garcat <gar@yeah.net>
2023-05-25 22:56:50 +1000

    lint

--- src/application/BlogViewPage/components/BlogTitle/BlogTitle.tsx
+++ src/application/BlogViewPage/components/BlogTitle/BlogTitle.tsx
@@ -17,20 +17,16 @@ const BlogTitle: FC = () => {
     <header className="md:w-[700px] mt-9 mb-8 md:mb-11 mx-auto lg:mx-0">
       <div className="flex flex-col sm:flex-row sm:justify-between text-sm text-on-background">
         <div className="flex">
-          <Date format={DateFormat.LongDate}>
-            {blog.createdAt}
-          </Date>
+          <Date format={DateFormat.LongDate}>{blog.createdAt}</Date>
           <div className="pl-2">-&nbsp;&nbsp;By&nbsp;{blog.user?.name}</div>
         </div>
         <div className="flex justify-end">
-          <div className="pr-4">{`9 Views`}</div>
-          
+          <div className="pr-4">9 Views</div>
           <div className="pr-4">
             <a href="#" title="Bookmark">
               <FiHeart className="inline w-5 h-5" />
             </a>
           </div>
-          
           <div className="pr-2">
             <a href="#" title="Share">
               <TbShare className="inline w-5 h-5" />
