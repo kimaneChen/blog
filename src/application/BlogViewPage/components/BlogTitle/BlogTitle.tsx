def06b51b2e111903afa324f7f0140c766a5bbfc
Garcat <gar@yeah.net>
2023-05-25 22:52:02 +1000

    Fix edit font and add design for views, bookmark and share.

--- src/application/BlogViewPage/components/BlogTitle/BlogTitle.tsx
+++ src/application/BlogViewPage/components/BlogTitle/BlogTitle.tsx
@@ -3,6 +3,8 @@ import Tag from '@/components/Tag'
 import DateFormat from '@/types/DateFormat'
 import { FC } from 'react'
 import useBlog from '@/hooks/useBlog'
+import { FiHeart } from 'react-icons/fi'
+import { TbShare } from 'react-icons/tb'
 
 const BlogTitle: FC = () => {
   const { blog } = useBlog()
@@ -13,9 +15,30 @@ const BlogTitle: FC = () => {
 
   return (
     <header className="md:w-[700px] mt-9 mb-8 md:mb-11 mx-auto lg:mx-0">
-      <Date className="text-sm text-on-background" format={DateFormat.LongDate}>
-        {blog.createdAt}
-      </Date>
+      <div className="flex flex-col sm:flex-row sm:justify-between text-sm text-on-background">
+        <div className="flex">
+          <Date format={DateFormat.LongDate}>
+            {blog.createdAt}
+          </Date>
+          <div className="pl-2">-&nbsp;&nbsp;By&nbsp;{blog.user?.name}</div>
+        </div>
+        <div className="flex justify-end">
+          <div className="pr-4">{`9 Views`}</div>
+          
+          <div className="pr-4">
+            <a href="#" title="Bookmark">
+              <FiHeart className="inline w-5 h-5" />
+            </a>
+          </div>
+          
+          <div className="pr-2">
+            <a href="#" title="Share">
+              <TbShare className="inline w-5 h-5" />
+            </a>
+          </div>
+        </div>
+      </div>
+
       <h1 className="font-bold text-3xl md:text-4xl my-3">{blog.title}</h1>
       {blog.description && (
         <h3 className="md:text-lg text-on-background mb-3">{blog.description}</h3>
